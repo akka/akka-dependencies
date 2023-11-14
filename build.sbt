beb4930e3c8226302f1aca2abd06a9f753e7f9e6
Enno Runne <458526+ennru@users.noreply.github.com>
2023-11-14 17:24:16 +0100

    Use Akka's scalafmt config

--- build.sbt
+++ build.sbt
@@ -8,11 +8,7 @@ ThisBuild / resolvers ++= sys.env
   .map { repo =>
     Seq(
       "lightbend-commercial-mvn".at(repo),
-      Resolver.url(
-        "lightbend-commercial-ivy",
-        url(repo)
-      )(Resolver.ivyStylePatterns)
-    )
+      Resolver.url("lightbend-commercial-ivy", url(repo))(Resolver.ivyStylePatterns))
   }
   .getOrElse(Seq.empty)
 
@@ -27,17 +23,13 @@ lazy val `akka-dependencies` =
       name := "akka-dependencies",
       description := s"${description.value} (depending on Scala ${CrossVersion.binaryScalaVersion(scalaVersion.value)})",
       homepage := Some(url("https://akka.io/")),
-      licenses := Seq(
-        ("BUSL-1.1", url("https://raw.githubusercontent.com/akka/akka-dependencies/v23.10.0/LICENSE"))
-      ),
+      licenses := Seq(("BUSL-1.1", url("https://raw.githubusercontent.com/akka/akka-dependencies/v23.10.0/LICENSE"))),
       developers := List(
-        Developer(
-          "akka-contributors",
-          "Akka Contributors",
-          "akka.official@gmail.com",
-          url("https://github.com/akka/akka-dependencies/graphs/contributors")
-        )
-      ),
+          Developer(
+            "akka-contributors",
+            "Akka Contributors",
+            "akka.official@gmail.com",
+            url("https://github.com/akka/akka-dependencies/graphs/contributors"))),
       // append -SNAPSHOT to version when isSnapshot
       ThisBuild / dynverSonatypeSnapshots := true,
       bomIncludeModules := akka ++
@@ -61,8 +53,7 @@ lazy val `akka-dependencies` =
         }
       },
       publishLocalConfiguration := publishLocalConfiguration.value.withOverwrite(true),
-      publishM2Configuration := publishM2Configuration.value.withOverwrite(true),
-    )
+      publishM2Configuration := publishM2Configuration.value.withOverwrite(true))
     .aggregate(docs)
 
 lazy val docs = project
@@ -74,29 +65,26 @@ lazy val docs = project
     projectInfoVersion := (if (isSnapshot.value) "snapshot" else version.value),
     publish / skip := true,
     paradoxProperties ++= Map(
-      "scala.binary.version" -> "2.13",
-      "akka-scala-2.13.version" -> "2.13",
-      "akka-scala-3.version" -> "3.3",
-      "akka-dependencies.version" -> version.value,
-      "akka-dependencies.major-version" -> "23.10",
-      "akka.version" -> Dependencies.Versions.Akka,
-      "akka-http.version" -> Dependencies.Versions.AkkaHttp,
-      "akka-grpc.version" -> Dependencies.Versions.AkkaGrpc,
-      "akka-management.version" -> Dependencies.Versions.AkkaManagement,
-      "akka-diagnostics.version" -> Dependencies.Versions.AkkaDiagnostics,
-      "akka-projections.version" -> Dependencies.Versions.AkkaProjections,
-      "akka-persistence-cassandra.version" -> Dependencies.Versions.AkkaPersistenceCassandra,
-      "akka-persistence-jdbc.version" -> Dependencies.Versions.AkkaPersistenceJdbc,
-      "akka-persistence-r2dbc.version" -> Dependencies.Versions.AkkaPersistenceR2dbc,
-      "alpakka.version" -> Dependencies.Versions.Alpakka,
-      "alpakka-kafka.version" -> Dependencies.Versions.AlpakkaKafka,
-    ),
+        "scala.binary.version" -> "2.13",
+        "akka-scala-2.13.version" -> "2.13",
+        "akka-scala-3.version" -> "3.3",
+        "akka-dependencies.version" -> version.value,
+        "akka-dependencies.major-version" -> "23.10",
+        "akka.version" -> Dependencies.Versions.Akka,
+        "akka-http.version" -> Dependencies.Versions.AkkaHttp,
+        "akka-grpc.version" -> Dependencies.Versions.AkkaGrpc,
+        "akka-management.version" -> Dependencies.Versions.AkkaManagement,
+        "akka-diagnostics.version" -> Dependencies.Versions.AkkaDiagnostics,
+        "akka-projections.version" -> Dependencies.Versions.AkkaProjections,
+        "akka-persistence-cassandra.version" -> Dependencies.Versions.AkkaPersistenceCassandra,
+        "akka-persistence-jdbc.version" -> Dependencies.Versions.AkkaPersistenceJdbc,
+        "akka-persistence-r2dbc.version" -> Dependencies.Versions.AkkaPersistenceR2dbc,
+        "alpakka.version" -> Dependencies.Versions.Alpakka,
+        "alpakka-kafka.version" -> Dependencies.Versions.AlpakkaKafka),
     paradoxRoots := List("index.html"),
     resolvers += Resolver.jcenterRepo,
     publishRsyncArtifacts += makeSite.value -> "www/",
-    publishRsyncHost := "akkarepo@gustav.akka.io"
-  )
-
+    publishRsyncHost := "akkarepo@gustav.akka.io")
 
 addCommandAlias("checkBom", ";scalafmtSbtCheck;+akka-dependencies/billOfMaterials:publishM2")
 addCommandAlias("checkPullBom", ";scalafmtSbtCheck;+update;+akka-dependencies/billOfMaterials:publishM2")
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,22 +2,22 @@ import sbt._
 
 object Dependencies {
   object Versions {
-    val Scala213           = "2.13.12"
+    val Scala213 = "2.13.12"
     val CrossScalaVersions = Seq(Scala213)
 
     // To update Cinnamon version, change the plugin version
     // in project/plugins.sbt
-    val Akka                     = "2.9.0"
-    val AkkaHttp                 = "10.6.0"
-    val AkkaManagement           = "1.5.0"
-    val AkkaProjections          = "1.5.0"
-    val AkkaGrpc                 = "2.4.0"
+    val Akka = "2.9.0"
+    val AkkaHttp = "10.6.0"
+    val AkkaManagement = "1.5.0"
+    val AkkaProjections = "1.5.0"
+    val AkkaGrpc = "2.4.0"
     val AkkaPersistenceCassandra = "1.2.0"
-    val AkkaPersistenceJdbc      = "5.3.0"
-    val AkkaPersistenceR2dbc     = "1.2.0"
-    val Alpakka                  = "7.0.0"
-    val AlpakkaKafka             = "5.0.0"
-    val AkkaDiagnostics          = "2.1.0"
+    val AkkaPersistenceJdbc = "5.3.0"
+    val AkkaPersistenceR2dbc = "1.2.0"
+    val Alpakka = "7.0.0"
+    val AlpakkaKafka = "5.0.0"
+    val AkkaDiagnostics = "2.1.0"
   }
 
   import Versions._
@@ -27,92 +27,81 @@ object Dependencies {
    * https://developer.lightbend.com/docs/introduction/getting-help/build-dependencies.html
    */
   val akka = Seq(
-    "com.typesafe.akka" %% "akka-actor"                  % Akka,
-    "com.typesafe.akka" %% "akka-actor-testkit-typed"    % Akka,
-    "com.typesafe.akka" %% "akka-actor-typed"            % Akka,
-    "com.typesafe.akka" %% "akka-cluster"                % Akka,
-    "com.typesafe.akka" %% "akka-cluster-metrics"        % Akka,
-    "com.typesafe.akka" %% "akka-cluster-sharding"       % Akka,
+    "com.typesafe.akka" %% "akka-actor" % Akka,
+    "com.typesafe.akka" %% "akka-actor-testkit-typed" % Akka,
+    "com.typesafe.akka" %% "akka-actor-typed" % Akka,
+    "com.typesafe.akka" %% "akka-cluster" % Akka,
+    "com.typesafe.akka" %% "akka-cluster-metrics" % Akka,
+    "com.typesafe.akka" %% "akka-cluster-sharding" % Akka,
     "com.typesafe.akka" %% "akka-cluster-sharding-typed" % Akka,
-    "com.typesafe.akka" %% "akka-cluster-tools"          % Akka,
-    "com.typesafe.akka" %% "akka-cluster-typed"          % Akka,
-    "com.typesafe.akka" %% "akka-coordination"           % Akka,
-    "com.typesafe.akka" %% "akka-discovery"              % Akka,
-    "com.typesafe.akka" %% "akka-distributed-data"       % Akka,
-    "com.typesafe.akka" %% "akka-serialization-jackson"  % Akka,
-    "com.typesafe.akka" %% "akka-multi-node-testkit"     % Akka,
-    "com.typesafe.akka" %% "akka-persistence"            % Akka,
-    "com.typesafe.akka" %% "akka-persistence-query"      % Akka,
-    "com.typesafe.akka" %% "akka-persistence-typed"      % Akka,
-    "com.typesafe.akka" %% "akka-persistence-testkit"    % Akka,
-    "com.typesafe.akka" %% "akka-protobuf-v3"            % Akka,
-    "com.typesafe.akka" %% "akka-pki"                    % Akka,
-    "com.typesafe.akka" %% "akka-remote"                 % Akka,
-    "com.typesafe.akka" %% "akka-slf4j"                  % Akka,
-    "com.typesafe.akka" %% "akka-stream"                 % Akka,
-    "com.typesafe.akka" %% "akka-stream-testkit"         % Akka,
-    "com.typesafe.akka" %% "akka-stream-typed"           % Akka,
-    "com.typesafe.akka" %% "akka-testkit"                % Akka
-  )
+    "com.typesafe.akka" %% "akka-cluster-tools" % Akka,
+    "com.typesafe.akka" %% "akka-cluster-typed" % Akka,
+    "com.typesafe.akka" %% "akka-coordination" % Akka,
+    "com.typesafe.akka" %% "akka-discovery" % Akka,
+    "com.typesafe.akka" %% "akka-distributed-data" % Akka,
+    "com.typesafe.akka" %% "akka-serialization-jackson" % Akka,
+    "com.typesafe.akka" %% "akka-multi-node-testkit" % Akka,
+    "com.typesafe.akka" %% "akka-persistence" % Akka,
+    "com.typesafe.akka" %% "akka-persistence-query" % Akka,
+    "com.typesafe.akka" %% "akka-persistence-typed" % Akka,
+    "com.typesafe.akka" %% "akka-persistence-testkit" % Akka,
+    "com.typesafe.akka" %% "akka-protobuf-v3" % Akka,
+    "com.typesafe.akka" %% "akka-pki" % Akka,
+    "com.typesafe.akka" %% "akka-remote" % Akka,
+    "com.typesafe.akka" %% "akka-slf4j" % Akka,
+    "com.typesafe.akka" %% "akka-stream" % Akka,
+    "com.typesafe.akka" %% "akka-stream-testkit" % Akka,
+    "com.typesafe.akka" %% "akka-stream-typed" % Akka,
+    "com.typesafe.akka" %% "akka-testkit" % Akka)
 
   val akkaHttp = Seq(
-    "com.typesafe.akka" %% "akka-http"            % AkkaHttp,
-    "com.typesafe.akka" %% "akka-http-caching"    % AkkaHttp,
-    "com.typesafe.akka" %% "akka-http-core"       % AkkaHttp,
-    "com.typesafe.akka" %% "akka-http-jackson"    % AkkaHttp,
+    "com.typesafe.akka" %% "akka-http" % AkkaHttp,
+    "com.typesafe.akka" %% "akka-http-caching" % AkkaHttp,
+    "com.typesafe.akka" %% "akka-http-core" % AkkaHttp,
+    "com.typesafe.akka" %% "akka-http-jackson" % AkkaHttp,
     "com.typesafe.akka" %% "akka-http-spray-json" % AkkaHttp,
-    "com.typesafe.akka" %% "akka-http-testkit"    % AkkaHttp,
-    "com.typesafe.akka" %% "akka-http-xml"        % AkkaHttp,
-    "com.typesafe.akka" %% "akka-parsing"         % AkkaHttp
-  )
+    "com.typesafe.akka" %% "akka-http-testkit" % AkkaHttp,
+    "com.typesafe.akka" %% "akka-http-xml" % AkkaHttp,
+    "com.typesafe.akka" %% "akka-parsing" % AkkaHttp)
 
   val akkaManagement = Seq(
-    "com.lightbend.akka.management" %% "akka-management"                   % AkkaManagement,
-    "com.lightbend.akka.management" %% "akka-management-cluster-http"      % AkkaManagement,
+    "com.lightbend.akka.management" %% "akka-management" % AkkaManagement,
+    "com.lightbend.akka.management" %% "akka-management-cluster-http" % AkkaManagement,
     "com.lightbend.akka.management" %% "akka-management-cluster-bootstrap" % AkkaManagement,
     "com.lightbend.akka.management" %% "akka-management-loglevels-logback" % AkkaManagement,
-    "com.lightbend.akka.management" %% "akka-lease-kubernetes"             % AkkaManagement,
-    "com.lightbend.akka.discovery"  %% "akka-discovery-kubernetes-api"     % AkkaManagement
-  )
+    "com.lightbend.akka.management" %% "akka-lease-kubernetes" % AkkaManagement,
+    "com.lightbend.akka.discovery" %% "akka-discovery-kubernetes-api" % AkkaManagement)
 
   val akkaProjections = Seq(
-    "com.lightbend.akka" %% "akka-projection-core"          % AkkaProjections,
-    "com.lightbend.akka" %% "akka-projection-eventsourced"  % AkkaProjections,
+    "com.lightbend.akka" %% "akka-projection-core" % AkkaProjections,
+    "com.lightbend.akka" %% "akka-projection-eventsourced" % AkkaProjections,
     "com.lightbend.akka" %% "akka-projection-durable-state" % AkkaProjections,
-    "com.lightbend.akka" %% "akka-projection-kafka"         % AkkaProjections,
-    "com.lightbend.akka" %% "akka-projection-cassandra"     % AkkaProjections,
-    "com.lightbend.akka" %% "akka-projection-jdbc"          % AkkaProjections,
-    "com.lightbend.akka" %% "akka-projection-r2dbc"         % AkkaProjections,
-    "com.lightbend.akka" %% "akka-projection-grpc"          % AkkaProjections,
-    "com.lightbend.akka" %% "akka-projection-testkit"       % AkkaProjections
-  )
+    "com.lightbend.akka" %% "akka-projection-kafka" % AkkaProjections,
+    "com.lightbend.akka" %% "akka-projection-cassandra" % AkkaProjections,
+    "com.lightbend.akka" %% "akka-projection-jdbc" % AkkaProjections,
+    "com.lightbend.akka" %% "akka-projection-r2dbc" % AkkaProjections,
+    "com.lightbend.akka" %% "akka-projection-grpc" % AkkaProjections,
+    "com.lightbend.akka" %% "akka-projection-testkit" % AkkaProjections)
 
-  val akkaGrpc = Seq(
-    "com.lightbend.akka.grpc" %% "akka-grpc-runtime" % AkkaGrpc
-  )
+  val akkaGrpc = Seq("com.lightbend.akka.grpc" %% "akka-grpc-runtime" % AkkaGrpc)
 
   val akkaPersistencePlugins = Seq(
-    "com.typesafe.akka"  %% "akka-persistence-cassandra" % AkkaPersistenceCassandra,
-    "com.lightbend.akka" %% "akka-persistence-jdbc"      % AkkaPersistenceJdbc,
-    "com.lightbend.akka" %% "akka-persistence-r2dbc"     % AkkaPersistenceR2dbc
-  )
+    "com.typesafe.akka" %% "akka-persistence-cassandra" % AkkaPersistenceCassandra,
+    "com.lightbend.akka" %% "akka-persistence-jdbc" % AkkaPersistenceJdbc,
+    "com.lightbend.akka" %% "akka-persistence-r2dbc" % AkkaPersistenceR2dbc)
 
   val alpakka = Seq(
-    "com.lightbend.akka" %% "akka-stream-alpakka-cassandra"      % Alpakka,
-    "com.lightbend.akka" %% "akka-stream-alpakka-csv"            % Alpakka,
-    "com.typesafe.akka"  %% "akka-stream-kafka"                  % AlpakkaKafka,
-    "com.typesafe.akka"  %% "akka-stream-kafka-cluster-sharding" % AlpakkaKafka,
-    "com.typesafe.akka"  %% "akka-stream-kafka-testkit"          % AlpakkaKafka
-  )
+    "com.lightbend.akka" %% "akka-stream-alpakka-cassandra" % Alpakka,
+    "com.lightbend.akka" %% "akka-stream-alpakka-csv" % Alpakka,
+    "com.typesafe.akka" %% "akka-stream-kafka" % AlpakkaKafka,
+    "com.typesafe.akka" %% "akka-stream-kafka-cluster-sharding" % AlpakkaKafka,
+    "com.typesafe.akka" %% "akka-stream-kafka-testkit" % AlpakkaKafka)
 
-  val akkaDiagnostics = Seq(
-    "com.lightbend.akka" %% "akka-diagnostics" % AkkaDiagnostics
-  )
+  val akkaDiagnostics = Seq("com.lightbend.akka" %% "akka-diagnostics" % AkkaDiagnostics)
 
   /**
    * Commercial components
    */
-
   // sbt-cinnamon plugin does not have artifacts for Scala 2.13.x
   val telemetry = com.lightbend.cinnamon.sbt.Cinnamon.library.modules.filterNot(_.name.equals("sbt-cinnamon"))
 
