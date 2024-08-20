4da05e08be587631b35afb75c6d60826fcd91a0b
Patrik Nordwall <patrik.nordwall@gmail.com>
2024-08-20 16:27:57 +0200

    bump: Akka 2.9.5 (#104)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
     // To update Cinnamon version, change the plugin version
     // in project/plugins.sbt
     val AkkaDependenciesMinor = "24.05"
-    val Akka = "2.9.4"
+    val Akka = "2.9.5"
     val AkkaHttp = "10.6.3"
     val AkkaManagement = "1.5.2"
     val AkkaProjections = "1.5.4"
