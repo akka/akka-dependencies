ddcc591f496cf7fe518909ab70a4fb8ad12b9bd2
Sebastian Alfers <sebastian.alfers@lightbend.com>
2024-11-04 20:09:26 +0100

    chore: add missing modules (#114)
    * chore: add missing modules

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -75,8 +75,14 @@ object Dependencies {
     "com.lightbend.akka.management" %% "akka-management-cluster-http" % AkkaManagement,
     "com.lightbend.akka.management" %% "akka-management-cluster-bootstrap" % AkkaManagement,
     "com.lightbend.akka.management" %% "akka-management-loglevels-logback" % AkkaManagement,
+    "com.lightbend.akka.management" %% "akka-management-loglevels-log4j2" % AkkaManagement,
+    "com.lightbend.akka.management" %% "akka-management-pki" % AkkaManagement,
     "com.lightbend.akka.management" %% "akka-lease-kubernetes" % AkkaManagement,
-    "com.lightbend.akka.discovery" %% "akka-discovery-kubernetes-api" % AkkaManagement)
+    "com.lightbend.akka.management" %% "akka-rolling-update-kubernetes" % AkkaManagement,
+    "com.lightbend.akka.discovery" %% "akka-discovery-kubernetes-api" % AkkaManagement,
+    "com.lightbend.akka.discovery" %% "akka-discovery-aws-api-async" % AkkaManagement,
+    "com.lightbend.akka.discovery" %% "akka-discovery-aws-api" % AkkaManagement,
+    "com.lightbend.akka.discovery" %% "akka-discovery-azure-api" % AkkaManagement)
 
   val akkaProjections = Seq(
     "com.lightbend.akka" %% "akka-projection-core" % AkkaProjections,
