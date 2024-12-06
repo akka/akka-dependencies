67fff291207dfc32789dc25e73411a748eef6a92
Peter Vlugter <59895+pvlugter@users.noreply.github.com>
2024-12-06 17:17:59 +1300

    bump: akka-persistence-dynamodb 2.0.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -16,7 +16,7 @@ object Dependencies {
     val AkkaProjections = "1.6.5"
     val AkkaGrpc = "2.5.0"
     val AkkaPersistenceCassandra = "1.3.0"
-    val AkkaPersistenceDynamoDb = "2.0.2"
+    val AkkaPersistenceDynamoDb = "2.0.3"
     val AkkaPersistenceJdbc = "5.5.0"
     val AkkaPersistenceR2dbc = "1.3.0"
     val Alpakka = "9.0.0"
