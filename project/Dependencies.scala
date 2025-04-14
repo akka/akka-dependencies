b2d69dab68408ebf8c3dc5bff6d2cc6516eb83c0
Johan Andrén <johan@markatta.com>
2025-04-14 10:45:33 +0200

    bump: Akka gRPC 2.5.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,7 +14,7 @@ object Dependencies {
     val AkkaHttp = "10.7.0"
     val AkkaManagement = "1.6.0"
     val AkkaProjections = "1.6.11"
-    val AkkaGrpc = "2.5.3"
+    val AkkaGrpc = "2.5.4"
     val AkkaPersistenceCassandra = "1.3.0"
     val AkkaPersistenceDynamoDb = "2.0.5"
     val AkkaPersistenceJdbc = "5.5.1"
