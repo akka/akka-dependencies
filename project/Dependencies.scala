1b9be96c9fd83fbacb016b97a3d4e521e463de55
Enno Runne <458526+ennru@users.noreply.github.com>
2026-08-03 13:35:13 +0200

    bump: Scala 2.13.18

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   object Versions {
-    val Scala213 = "2.13.17"
+    val Scala213 = "2.13.18"
     val Scala3 = "3.3.7"
     val CrossScalaVersions = Seq(Scala213, Scala3)
 
