84b02d790cc488d11b205d75952ba20fad8307d1
Sebastian Alfers <sebastian.alfers@lightbend.com>
2026-08-12 10:13:36 +0200

    chore: bump akka-core to 2.10.21

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
     // To update Cinnamon version, change the plugin version
     // in project/plugins.sbt
     val AkkaDependenciesMinor = "25.10"
-    val Akka = "2.10.20"
+    val Akka = "2.10.21"
     val AkkaHttp = "10.7.4"
     val AkkaManagement = "1.6.5"
     val AkkaProjections = "1.6.23"
